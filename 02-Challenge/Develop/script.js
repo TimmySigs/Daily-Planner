0c1006912f95e4c63df92dacbd659ddd6d4cc21d
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-08-17 23:33:14 -0500

    "changed files around, setup a var for day"

--- 02-Challenge/Develop/script.js
+++
@@ -1,6 +0,0 @@
-// Setting up the date on top //
-var a= dayjs().format('dddd MMMM D YYYY, h:mm');
-  $("#display-date").text(a)
-  var row = ""
-
-  
---
+++ script.js
@@ -0,0 +1,49 @@
+// Setting up the date on top //
+var a= dayjs().format('dddd MMMM D YYYY, h:mm');
+  $("#display-date").text(a)
+  var row = ""
+
+  var theDay = [
+    {
+        id: "0",
+        hour: "09",
+        time: "09",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "1",
+        hour: "10",
+        time: "10",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "2",
+        hour: "11",
+        time: "11",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "3",
+        hour: "12",
+        time: "12",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "4",
+        hour: "01",
+        time: "13",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "5",
+        hour: "02",
+        time: "14",
+        meridiem: "pm",
+        reminder: ""
+    }, 
+]
